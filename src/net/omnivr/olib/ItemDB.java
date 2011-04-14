5930e2c6baf50651ae5d997dfafe63d1b4623de2
Nayruden <nayruden@gmail.com>
2011-04-13 23:55:23 -0500

    Update ItemDB to support different durabilities

--- src/net/omnivr/olib/ItemDB.java
+++ src/net/omnivr/olib/ItemDB.java
@@ -3,6 +3,7 @@ package net.omnivr.olib;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  *
@@ -10,21 +11,47 @@ import java.util.Scanner;
  */
 public class ItemDB {
 
-    static Map<String, Integer> items;
+    public static class ItemIDAndDurability {
 
-    public static int nameOrIDToID(String name) {
+        final int id;
+        final int durability;
+
+        ItemIDAndDurability(int id, int durability) {
+            this.id = id;
+            this.durability = durability;
+        }
+
+        public int getID() {
+            return id;
+        }
+
+        public int getDurability() {
+            Boolean durability_ = durability_important.get(id);
+            if (durability_ != null && durability_) {
+                return durability;
+            }
+            return -1;
+        }
+    }
+    static Map<String, ItemIDAndDurability> items;
+    static Map<Integer, Boolean> durability_important;
+
+    public static ItemIDAndDurability nameOrIDToID(String name) {
         ensureDBLoaded();
 
-        Integer id = null;
+        ItemIDAndDurability item = null;
         try {
-            id = Integer.parseInt(name); // Assume it's an id first
-        } catch (NumberFormatException e) {
-            id = items.get(name); // Maybe it's a name
-        } finally {
-            if (id == null || !items.containsValue(id.intValue())) {
-                return -1;
+            String pieces[] = name.split(",", 2);
+            int id = Integer.parseInt(pieces[0]); // Assume it's an id first
+            if (pieces.length == 2 && durability_important.containsKey(id)) {
+                item = new ItemIDAndDurability(id, Integer.parseInt(pieces[1]));
+            } else {
+                item = new ItemIDAndDurability(id, 0);
             }
-            return id.intValue();
+        } catch (NumberFormatException e) {
+            item = items.get(name); // Maybe it's a name
+        } finally {
+            return item;
         }
     }
 
@@ -33,7 +60,8 @@ public class ItemDB {
             return;
         }
 
-        items = new HashMap<String, Integer>();
+        items = new HashMap<String, ItemIDAndDurability>();
+        durability_important = new TreeMap<Integer, Boolean>();
         Scanner scanner = new Scanner(ItemDB.class.getResourceAsStream("/items.db"));
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine().trim();
@@ -41,10 +69,15 @@ public class ItemDB {
                 continue;
             }
 
-            String pieces[] = line.split(":", 2);
+            String pieces[] = line.split(",", 3);
             try {
                 int item_id = Integer.parseInt(pieces[1]);
-                items.put(pieces[0], item_id);
+                int item_durability = pieces.length < 3 ? 0 : Integer.parseInt(pieces[2]);
+                items.put(pieces[0], new ItemIDAndDurability(item_id, item_durability));
+                Boolean durability = durability_important.get(item_id);
+                if (item_durability > 0 && (durability == null || !durability)) {
+                    durability_important.put(item_id, Boolean.TRUE);
+                }
             } catch (NumberFormatException e) { // Ignore
             }
         }
